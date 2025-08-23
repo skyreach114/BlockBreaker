1fe65fea530f61fba3118b69761a38de3c2e2e85
skyreach114 <isisu1020@gmail.com>
2025-08-23 23:42:47 +0900

    ・パドルの左右移動を実装、Clampで壁制御を追加 ・背景をタイル化して調整、パドルの当たり判定を設定

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
+    public float speed = 10f;
+    private Vector2 moveInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, 0);
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+        // 壁の内側で制限
+        float clampedX = Mathf.Clamp(transform.position.x, -2.7f, 2.7f);
+        transform.position = new Vector3(clampedX, transform.position.y, 0);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
