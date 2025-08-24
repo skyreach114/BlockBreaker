b5ba602207e3ef77a97a1bfd43f552f467c0765a
skyreach114 <isisu1020@gmail.com>
2025-08-24 23:14:18 +0900

    ボールの移動・壁との反射・GameOver表示を実装

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 8.5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        float angle = Random.Range(20f, 160f);
+        Vector2 dir = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        rb.velocity = dir.normalized * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = rb.velocity.normalized * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            // ボールの現在の速度を取得
+            Vector2 velocity = rb.velocity;
+
+            // パドルの速度を取得（あらかじめRigidbody2Dを持たせるか、スクリプトから参照）
+            float paddleVelocityX = collision.rigidbody != null ? collision.rigidbody.velocity.x : 0f;
+
+            // 横方向の力を少しだけ加える
+            velocity.x += paddleVelocityX * 0.1f;
+
+            // 再正規化して一定速度に保つ
+            velocity = velocity.normalized * speed;
+
+            rb.velocity = velocity;
+        }
+    }
+
+}
